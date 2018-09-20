55e0692a082924bec14d1848fd1cccb44c26b402
urdan <urdans>
2018-09-19 20:57:43 -0400

    Completed the studio class

--- main.py
+++ main.py
@@ -37,17 +37,48 @@ add_form = """
 # Name the action for the form '/crossoff' and make its method 'post'.
 
 # a form for crossing off watched movies
+# crossoff_form = """
+#     <form action="/crossoff" method="post">
+#         <label for="crossed">
+#             I want to cross off
+#             <input type="text" id="crossed" name="crossed-off-movie"/>
+#             from my watchlist.
+#         </label>
+#         <input type="submit" value="Do it"/>
+#     </form>
+# """
 crossoff_form = """
-
+    <form action="/crossoff" method="post">
+        <!-- <label for="crossed"> -->
+            I want to cross off
+            <select id="crossed" name="crossed-off-movie">
+            {}
+            </select>    
+            from my watchlist.
+        <!-- </label> -->
+        <input type="submit" value="Do it"/>
+    </form>
 """
+
+watchlist = ["Star Trek", "Stars War", "The Schinler List", "Casa Blanca", "The Incredibles I"]
 
 # TODO:
 # Finish filling in the function below so that the user will see a message like:
 # "Star Wars has been crossed off your watchlist".
 # And create a route above the function definition to receive and handle the request from 
 # your crossoff_form.
+
+
+@app.route("/crossoff", methods=['POST'])
 def crossoff_movie():
     crossed_off_movie = request.form['crossed-off-movie']    
+                #     <option value = "option" > Option 1 < /option >
+                # <option value="option">Option 2</option
+    print(watchlist)
+    print(crossed_off_movie)
+    watchlist.remove(crossed_off_movie)
+    return "<strike>{}</strike> has been crossed off from your watchlist.".format(crossed_off_movie)
+
 
 # TODO:
 # modify the crossoff_form above to use a dropdown (<select>) instead of
@@ -56,7 +87,7 @@ def crossoff_movie():
 @app.route("/add", methods=['POST'])
 def add_movie():
     new_movie = request.form['new-movie']
-
+    watchlist.append(new_movie)
     # build response content
     new_movie_element = "<strong>" + new_movie + "</strong>"
     sentence = new_movie_element + " has been added to your Watchlist!"
@@ -70,7 +101,13 @@ def index():
     edit_header = "<h2>Edit My Watchlist</h2>"
 
     # build the response string
-    content = page_header + edit_header + add_form + page_footer
+    # building the options
+    op = ""    
+    for movie in watchlist:
+    	 op = op + '<option value="' + movie +'">' + movie + '</option>\n'
+
+    content = page_header + edit_header + add_form + \
+        "<br>"*3 + crossoff_form.format(op) + page_footer
 
     return content
 
